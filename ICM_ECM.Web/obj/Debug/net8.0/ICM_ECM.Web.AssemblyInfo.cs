fe2690ebf5d645a58cb5cd4b0fe7958f9f7ec7f4
Khareem O'Brain <kareem.olayinka@outlook.com>
2024-10-20 13:45:13 +0100

    minors

--- ICM_ECM.Web/obj/Debug/net8.0/ICM_ECM.Web.AssemblyInfo.cs
+++ ICM_ECM.Web/obj/Debug/net8.0/ICM_ECM.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ICM_ECM.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e77743b8fe16cc695bade206b8a8dd6bfa57dfae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a88315fae31eabf1f7d40231dc71a47acc37ca3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ICM_ECM.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ICM_ECM.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
